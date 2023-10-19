9a3e4d6fdd7032e860e8be15706ffc87c6c640b2
Kranthikumarm786 <kranthikumarm786@gmail.com>
2023-10-19 13:07:18 +0530

    third commit

--- artcles/models.py
+++ artcles/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.db.models import Q
 from django.db.models.signals import pre_save, post_save
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -7,13 +9,33 @@ from django.utils import timezone
 
 from artcles.utils import slugify_instance_title
 
+class ArticleQuerySet(models.QuerySet):
+    def search(self,query=None):
+        if query is None or query == "":
+            return self.none()  #[]
+        lookups = Q(title__icontains=query) | Q(content__icontains=query)
+        return self.filter(lookups)
+
+class ArticleManager(models.Manager):
+    def get_queryset(self):
+        return ArticleQuerySet(self.model, using=self._db)
+
+    def search(self,query=None):
+        return self.get_queryset().search(query=query)
+
 class Article(models.Model):
     title=models.CharField(max_length=120)
     slug=models.SlugField(unique=True,blank=True, null=True)
     content=models.TextField()
     timestamp=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now=True)
-    #publish=models.DateField(auto_now_add=False,auto_now=False, null=True,blank=True)
+    publish=models.DateField(auto_now_add=False,auto_now=False, null=True,blank=True)
+
+    objects=ArticleManager()
+
+    def get_absolute_url(self):
+        #return f'/artcles/{self.slug}/'
+        return reverse("article-detail", kwargs={"slug": self.slug})
 
     def save(self, *args, **kwargs):
         #obj=Article.objects.get(id=1)
