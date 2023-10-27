a93545be4f2a50a18dd5ab9491e0a8a0199e71c7
Kranthikumarm786 <kranthikumarm786@gmail.com>
2023-10-27 06:26:58 +0530

    fifty five vedio comiit

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,8 @@
+import pint
 from django.conf import settings
 from django.db import models
 
+from.utils import number_str_to_float
 from .validators import validate_unit_of_measure
 """
 -Global
@@ -27,6 +29,7 @@ class RecipeIngredient(models.Model):
     name=models.CharField(max_length=220)
     description=models.TextField(blank=True, null=True)
     quantity=models.CharField(max_length=50)   #1 1/4
+    quantity_as_float=models.FloatField(blank=True, null=True)
     # pounds, lbs,oz,gram,etc
     unit=models.CharField(max_length=50, validators=[validate_unit_of_measure])  #pounds, lbs,oz,gram,etc
     direction=models.TextField(blank=True, null=True)
@@ -34,5 +37,26 @@ class RecipeIngredient(models.Model):
     updated =models.DateTimeField(auto_now=True)
     active=models.BooleanField(default=True)
 
+    def convert_to_system(self, system="mks"):
+        ureg=pint.UnitRegistry(system=system)
+        measurement=self.quantity_as_float * ureg[self.unit]
+        print(measurement)
+
+    def as_mks(self):
+        #meter, kilogram,second
+        pass
+    def as_imperial(self):
+        #miles, pounds, seconds
+        pass
+
+
+    def save(self, *args, **kwargs):
+        qty=self.quantity
+        qty_as_float, qty_as_float_success=number_str_to_float(qty)
+        if qty_as_float_success:
+            self.quantity_as_float=qty_as_float
+        else:
+            self.quantity_as_float=None
+        super().save(*args, **kwargs)
 
 
--- recipes/tests.py
+++ recipes/tests.py
@@ -32,6 +32,12 @@ class RecipeTestCase(TestCase):
             quantity='1/2',
             unit='pound'
         )
+        self.recipe_ingredient_b = RecipeIngredient.objects.create(
+            recipe=self.recipe_a,
+            name='Chicken',
+            quantity='asdfghjk',
+            unit='pound'
+        )
 
 
     def test_user_count(self):
@@ -52,29 +58,29 @@ class RecipeTestCase(TestCase):
     def test_user_ingredient_recipe_reverse_count(self):
         recipe=self.recipe_a
         qs=recipe.recipeingredient_set.all()
-        self.assertEqual(qs.count(),1)
+        self.assertEqual(qs.count(),2)
 
     def test_user_ingredientcount(self):
         recipe=self.recipe_a
         qs=RecipeIngredient.objects.filter(recipe=recipe)
-        self.assertEqual(qs.count(),1)
+        self.assertEqual(qs.count(),2)
 
     def test_user_two_level_relation(self):
         user=self.user_a
         qs=RecipeIngredient.objects.filter(recipe__user=user)
-        self.assertEqual(qs.count(),1)
+        self.assertEqual(qs.count(),2)
 
     def test_user_two_level_relation_reverse(self):
         user=self.user_a
         recipeingredient_ids=list(user.recipe_set.all().values_list('recipeingredient__id',flat=True))
         qs=RecipeIngredient.objects.filter(id__in=recipeingredient_ids)
-        self.assertEqual(qs.count(),1)
+        self.assertEqual(qs.count(),2)
 
     def test_user_two_level_relation_via_recipes(self):
         user=self.user_a
         ids=user.recipe_set.all().values_list("id", flat=True)
         qs=RecipeIngredient.objects.filter(recipe__id__in=ids)
-        self.assertEqual(qs.count(),1)
+        self.assertEqual(qs.count(),2)
 
     def test_unit_measure_validation(self):
         invalid_unit='ounce'
@@ -97,3 +103,7 @@ class RecipeTestCase(TestCase):
                     unit=unit
                 )
                 ingredient.full_clean()
+
+    def test_quantity_as_float(self):
+        self.assertIsNotNull(self.recipe_ingredient_a.quantity_as_float)
+        self.assertIsNull(self.recipe_ingredient_b.quantity_as_float)
