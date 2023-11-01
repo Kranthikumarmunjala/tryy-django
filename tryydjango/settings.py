aa19228b3cce9e2961e95976ac5c8962e414bff3
Kranthikumarm786 <kranthikumarm786@gmail.com>
2023-11-01 14:57:14 +0530

    seventy five video commit

--- tryydjango/settings.py
+++ tryydjango/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third-party
     'django_htmx',
+    'storages',
+    # internal
     'artcles',
     'recipes',
     'search',
@@ -132,6 +135,8 @@ STATICFILES_DIRS=[
 ]
 
 STATIC_ROOT=BASE_DIR/"staticfiles-cdn" #in production, we want cdn
+
+from.cdn.conf import * #noqa
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
