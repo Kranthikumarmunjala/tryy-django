a6fbc5ec35bb5c5934d39b09f9aaad88fd8ec39d
Kranthikumarm786 <kranthikumarm786@gmail.com>
2023-11-01 06:31:59 +0530

    seventy two vedio comiit

--- recipes/models.py
+++ recipes/models.py
@@ -101,6 +101,9 @@ class Recipe(models.Model):
     def get_edit_url(self):
         return reverse("recipess:update", kwargs={"id": self.id})
 
+    def get_delete_url(self):
+        return reverse("recipes:delete", kwargs={"id":self.id})
+
     def get_ingrediets_chidren(self):
         return self.recipeingredient_set.filter()
 
@@ -119,6 +122,13 @@ class RecipeIngredient(models.Model):
 
     def get_absolute_url(self):
         return self.recipe.get_absolute_url()
+
+    def get_delete_url(self):
+        kwargs = {
+            "parent_id": self.recipe.id,
+            "id": self.id
+        }
+        return reverse("recipes:ingredient-delete", kwargs=kwargs)
 
     def get_hx_edit_url(self):
         kwargs={
